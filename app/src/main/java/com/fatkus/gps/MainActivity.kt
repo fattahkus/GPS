fa1f0b59456f984d669caeda92b6b4029f8a28c1
Fattah <irfan.fattah.kusuma@student.uty.ac.id>
2019-12-09 13:56:13 +0800

    v1

---
+++ app/src/main/java/com/fatkus/gps/MainActivity.kt
@@ -0,0 +1,93 @@
+package com.fatkus.gps
+
+import android.content.pm.PackageManager
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Looper
+import android.view.View
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import com.google.android.gms.location.*
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+    lateinit var fusedLocationProviderClient: FusedLocationProviderClient
+    lateinit var locationRequest: LocationRequest
+    lateinit var locationCallback: LocationCallback
+        val REQUEST_CODE = 100
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        when(requestCode){
+            REQUEST_CODE->{
+                if(grantResults.size>0) {
+                    if (grantResults[0] == PackageManager.PERMISSION_GRANTED)
+                        Toast.makeText(
+                            this@MainActivity,
+                            "permission granted",
+                            Toast.LENGTH_SHORT
+                        ).show()
+                    else
+                        Toast.makeText(
+                            this@MainActivity,
+                            "permission denied",
+                            Toast.LENGTH_SHORT
+                        ).show()
+                }
+            }
+        }
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        if(ActivityCompat.shouldShowRequestPermissionRationale(this,android.Manifest.permission.ACCESS_FINE_LOCATION))
+            ActivityCompat.requestPermissions(this, arrayOf(android.Manifest.permission.ACCESS_FINE_LOCATION),REQUEST_CODE)
+        else{
+            buildLocationRequest()
+            buildLocationonCallBack()
+
+            fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this)
+
+            btnUpdate.setOnClickListener(View.OnClickListener {
+                if(ActivityCompat.checkSelfPermission(this@MainActivity,android.Manifest.permission.ACCESS_FINE_LOCATION)!= PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this@MainActivity,android.Manifest.permission.ACCESS_COARSE_LOCATION)!= PackageManager.PERMISSION_GRANTED){
+                    ActivityCompat.requestPermissions(this@MainActivity,arrayOf(android.Manifest.permission.ACCESS_FINE_LOCATION),REQUEST_CODE)
+                    return@OnClickListener
+                }
+                fusedLocationProviderClient.requestLocationUpdates(locationRequest,locationCallback,
+                    Looper.myLooper())
+                    btnUpdate.isEnabled = !btnUpdate.isEnabled
+                    btnStop.isEnabled = !btnStop.isEnabled
+            })
+            btnStop.setOnClickListener(View.OnClickListener {
+                if (ActivityCompat.checkSelfPermission(this@MainActivity,android.Manifest.permission.ACCESS_FINE_LOCATION)!= PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this@MainActivity,android.Manifest.permission.ACCESS_COARSE_LOCATION)!= PackageManager.PERMISSION_GRANTED){
+                    ActivityCompat.requestPermissions(this@MainActivity,
+                        arrayOf(android.Manifest.permission.ACCESS_FINE_LOCATION),REQUEST_CODE)
+                    return@OnClickListener
+                }
+                fusedLocationProviderClient.removeLocationUpdates(locationCallback)
+                btnUpdate.isEnabled = !btnStop.isEnabled
+                btnStop.isEnabled = !btnStop.isEnabled
+            })
+        }
+    }
+    private fun buildLocationonCallBack(){
+        locationCallback = object :LocationCallback(){
+            override fun onLocationResult(p0: LocationResult?) {
+                var location = p0!!.locations.get(p0!!.locations.size-1)
+                txLocation.text = location.latitude.toString()+"/"+location.longitude.toString()
+            }
+        }
+    }
+    private fun buildLocationRequest(){
+        locationRequest = LocationRequest()
+        locationRequest.priority = LocationRequest.PRIORITY_HIGH_ACCURACY
+        locationRequest.fastestInterval = 3000
+        locationRequest.smallestDisplacement = 10f
+    }
+}
